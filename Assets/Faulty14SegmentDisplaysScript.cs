76abf5cf94d63b66d838c3dfd8a176216f8c0c27
JakkOfKlubs <qwuest123@gmail.com>
2021-12-02 17:02:33 -0500

    Final touches.

--- Assets/Faulty14SegmentDisplaysScript.cs
+++ Assets/Faulty14SegmentDisplaysScript.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 using KModkit;
 using Rnd = UnityEngine.Random;
 using System;
+using System.Text.RegularExpressions;
 
 public class Faulty14SegmentDisplaysScript : MonoBehaviour
 {
@@ -72,6 +73,7 @@ public class Faulty14SegmentDisplaysScript : MonoBehaviour
     private int _currentSelectedColor;
     private int _currentSelectedSegment = 99;
     private bool _segIsSelected;
+    private bool _isAnimating;
 
     private void Start()
     {
@@ -79,16 +81,22 @@ public class Faulty14SegmentDisplaysScript : MonoBehaviour
         _currentRSequenceIx = Rnd.Range(0, 26);
         _currentGSequenceIx = Rnd.Range(0, 26);
         _currentBSequenceIx = Rnd.Range(0, 26);
-        _rSegPositions = Enumerable.Range(0, 14).ToArray().Shuffle();
-        Debug.LogFormat("[Faulty 14 Segment Displays #{0}] Shuffled red segment order: {1}", _moduleId, _rSegPositions.Select(i => i + 1).Join(" "));
-        _gSegPositions = Enumerable.Range(0, 14).ToArray().Shuffle();
-        Debug.LogFormat("[Faulty 14 Segment Displays #{0}] Shuffled green segment order: {1}", _moduleId, _gSegPositions.Select(i => i + 1).Join(" "));
-        _bSegPositions = Enumerable.Range(0, 14).ToArray().Shuffle();
-        Debug.LogFormat("[Faulty 14 Segment Displays #{0}] Shuffled green segment order: {1}", _moduleId, _bSegPositions.Select(i => i + 1).Join(" "));
+        _rSegPositions = Enumerable.Range(0, 14).ToArray();
+        _gSegPositions = Enumerable.Range(0, 14).ToArray();
+        _bSegPositions = Enumerable.Range(0, 14).ToArray();
+
+        _rSegPositions.Shuffle();
+        _gSegPositions.Shuffle();
+        _bSegPositions.Shuffle();
+
         _cycleSequence = StartCoroutine(CycleSequence());
+        Debug.LogFormat("[Faulty 14 Segment Displays #{0}] Shuffled red segment order: {1}", _moduleId, _rSegPositions.Select(i => i + 1).Join(" "));
+        Debug.LogFormat("[Faulty 14 Segment Displays #{0}] Shuffled green segment order: {1}", _moduleId, _gSegPositions.Select(i => i + 1).Join(" "));
+        Debug.LogFormat("[Faulty 14 Segment Displays #{0}] Shuffled blue segment order: {1}", _moduleId, _bSegPositions.Select(i => i + 1).Join(" "));
 
         PlayPauseSel.OnInteract += delegate ()
         {
+            Audio.PlayGameSoundAtTransform(KMSoundOverride.SoundEffect.ButtonPress, transform);
             if (!_moduleSolved)
             {
                 if (_isCycling)
@@ -110,6 +118,7 @@ public class Faulty14SegmentDisplaysScript : MonoBehaviour
 
         LeftSel.OnInteract += delegate ()
         {
+            Audio.PlayGameSoundAtTransform(KMSoundOverride.SoundEffect.ButtonPress, transform);
             if (!_moduleSolved)
             {
                 if (!_isCycling)
@@ -129,6 +138,7 @@ public class Faulty14SegmentDisplaysScript : MonoBehaviour
 
         RightSel.OnInteract += delegate ()
         {
+            Audio.PlayGameSoundAtTransform(KMSoundOverride.SoundEffect.ButtonPress, transform);
             if (!_moduleSolved)
             {
                 if (!_isCycling)
@@ -157,19 +167,34 @@ public class Faulty14SegmentDisplaysScript : MonoBehaviour
 
     private bool SubmitPress()
     {
-        bool correct = true;
+        Audio.PlayGameSoundAtTransform(KMSoundOverride.SoundEffect.ButtonPress, transform);
+        if (_isAnimating)
+            return false;
+        var correct = new int[14];
         for (int i = 0; i < 14; i++)
         {
             if (_rSegPositions[i] != i || _gSegPositions[i] != i || _bSegPositions[i] != i)
-            {
-                correct = false;
-                break;
-            }
+                correct[i] = 1;
         }
-        if (correct)
-            Module.HandlePass();
-        else
+        Debug.LogFormat("[Faulty 14 Segment Displays #{0}] Submitted red segments: {1}", _moduleId, _rSegPositions.Select(i => i + 1).Join(" "));
+        Debug.LogFormat("[Faulty 14 Segment Displays #{0}] Submitted green segments: {1}", _moduleId, _gSegPositions.Select(i => i + 1).Join(" "));
+        Debug.LogFormat("[Faulty 14 Segment Displays #{0}] Submitted blue segments: {1}", _moduleId, _bSegPositions.Select(i => i + 1).Join(" "));
+        _isAnimating = true;
+        if (correct.Contains(1))
+        {
+            if (_cycleSequence != null)
+                StopCoroutine(_cycleSequence);
+            for (int i = 0; i < SegmentObjs.Length; i++)
+                SegmentObjs[i].GetComponent<MeshRenderer>().material = SegmentMats[correct[i] == 0 ? 2 : 4];
             Module.HandleStrike();
+            StartCoroutine(StrikeAnimation());
+            Debug.LogFormat("[Faulty 14 Segment Displays #{0}] Not all color channels have been correctly swapped. Strike.", _moduleId);
+        }
+        else
+        {;
+            StartCoroutine(SolveAnimation());
+            Debug.LogFormat("[Faulty 14 Segment Displays #{0}] All color channels have been correctly swapped. Module solved.", _moduleId);
+        }
         return false;
     }
 
@@ -177,6 +202,8 @@ public class Faulty14SegmentDisplaysScript : MonoBehaviour
     {
         return delegate ()
         {
+            var soundNames = new string[] { "SegSelect1", "SegSelect2", "SegSelect3", "SegSelect4", "SegSelect5" };
+            Audio.PlaySoundAtTransform(soundNames[Rnd.Range(0, soundNames.Length)], transform);
             if (!_moduleSolved)
             {
                 _currentSelectedColor = color;
@@ -190,6 +217,10 @@ public class Faulty14SegmentDisplaysScript : MonoBehaviour
     {
         return delegate ()
         {
+            if (_isAnimating)
+                return false;
+            var soundNames = new string[] { "SegSelect1", "SegSelect2", "SegSelect3", "SegSelect4", "SegSelect5" };
+            Audio.PlaySoundAtTransform(soundNames[Rnd.Range(0, soundNames.Length)], transform);
             if (!_segIsSelected)
             {
                 _segIsSelected = true;
@@ -254,5 +285,171 @@ public class Faulty14SegmentDisplaysScript : MonoBehaviour
             _currentGSequenceIx = (_currentGSequenceIx + 1) % 26;
             _currentBSequenceIx = (_currentBSequenceIx + 1) % 26;
         }
+    }
+
+    private IEnumerator StrikeAnimation()
+    {
+        yield return new WaitForSeconds(3);
+        _cycleSequence = StartCoroutine(CycleSequence());
+        PlayPauseText.text = "PAUSE";
+        _isCycling = true;
+        _isAnimating = false;
+    }
+
+    private IEnumerator SolveAnimation()
+    {
+        Audio.PlaySoundAtTransform("InputCorrect", transform);
+        if (_cycleSequence != null)
+            StopCoroutine(_cycleSequence);
+        for (int i = 0; i < SegmentObjs.Length; i++)
+            SegmentObjs[i].GetComponent<MeshRenderer>().material = SegmentMats[0];
+        var solveSegs = new bool[15][]
+        {
+            new bool[14] { true, true, false, false, false, false, false, false, true, false, false, false, false, true }, //C
+            new bool[14] { true, true, false, false, false, true, false, false, true, false, false, false, true, true },   //O
+            new bool[14] { false, true, true, false, false, true, false, false, true, false, false, true, true, false },   //N
+            new bool[14] { true, true, false, false, false, false, false, true, true, false, false, false, true, true },   //G
+            new bool[14] { true, true, false, false, false, true, true, true, true, false, false, true, false, false },    //R
+            new bool[14] { true, true, false, false, false, true, true, true, true, false, false, false, true, false },    //A
+            new bool[14] { true, false, false, true, false, false, false, false, false, false, true, false, false, false },//T
+            new bool[14] { false, true, false, false, false, true, false, false, true, false, false, false, true, true },  //U
+            new bool[14] { false, true, false, false, false, false, false, false, true, false, false, false, false, true },//L
+            new bool[14] { true, true, false, false, false, true, true, true, true, false, false, false, true, false },    //A
+            new bool[14] { true, false, false, true, false, false, false, false, false, false, true, false, false, false },//T
+            new bool[14] { true, false, false, true, false, false, false, false, false, false, true, false, false, true }, //I
+            new bool[14] { true, true, false, false, false, true, false, false, true, false, false, false, true, true },   //O
+            new bool[14] { false, true, true, false, false, true, false, false, true, false, false, true, true, false },   //N
+            new bool[14] { true, true, false, false, false, false, true, true, false, false, false, false, true, true },   //S
+        };
+        var dashSegs = new bool[14] { false, false, false, false, false, false, true, true, false, false, false, false, false, false };
+        for (int j = 0; j < solveSegs.Length; j++) {
+            for (int i = 0; i < SegmentObjs.Length; i++)
+                SegmentObjs[i].GetComponent<MeshRenderer>().material = SegmentMats[solveSegs[j][i] ? 2 : 0];
+            yield return new WaitForSeconds(0.15f);
+        }
+        _moduleSolved = true;
+        Module.HandlePass();
+        for (int i = 0; i < SegmentObjs.Length; i++)
+            SegmentObjs[i].GetComponent<MeshRenderer>().material = SegmentMats[dashSegs[i] ? 2 : 0];
+    }
+
+#pragma warning disable 0414
+    private readonly string TwitchHelpMessage = "!{0} swap 1 14 [Swap segments 1 and 14] | !{0} red [Pick colors red/green/blue] | !{0} toggle [Pauses/resumes the cycle] | !{0} left/right [Cycle left/right in the sequence] | !{0} submit [Submit the answer]";
+#pragma warning restore 0414
+
+    private IEnumerator ProcessTwitchCommand(string command)
+    {
+        Match m;
+        m = Regex.Match(command, @"^\s*swap\s*(\d+)\s*(\d+)\s*$", RegexOptions.IgnoreCase | RegexOptions.CultureInvariant);
+        if (m.Success)
+        {
+            yield return null;
+            Debug.LogFormat("swap {0} {1}", m.Groups[1].Value, m.Groups[2].Value);
+            int val1;
+            int val2;
+            if (!int.TryParse(m.Groups[1].Value, out val1) || !int.TryParse(m.Groups[2].Value, out val2))
+            {
+                yield return "sendtochaterror Invalid segments! Must be in the range from 1 to 14";
+                yield break;
+            }
+            if (val1 > 14 || val2 > 14)
+            {
+                yield return "sendtochaterror Invalid segments! Must be in the range from 1 to 14";
+                yield break;
+            }
+            SegmentSels[val1 - 1].OnInteract();
+            yield return new WaitForSeconds(0.2f);
+            SegmentSels[val2 - 1].OnInteract();
+            yield return new WaitForSeconds(0.1f);
+            yield break;
+        }
+        m = Regex.Match(command, @"^\s*(red|green|blue)\s*$", RegexOptions.IgnoreCase | RegexOptions.CultureInvariant);
+        if (m.Success)
+        {
+            yield return null;
+            var col = m.Groups[1].ToString();
+            if (col == "red")
+                ColorPickerSels[0].OnInteract();
+            else if (col == "green")
+                ColorPickerSels[1].OnInteract();
+            else if (col == "blue")
+                ColorPickerSels[2].OnInteract();
+            yield return new WaitForSeconds(0.1f);
+            yield break;
+        }
+        m = Regex.Match(command, @"^\s*(left|right)\s*$", RegexOptions.IgnoreCase | RegexOptions.CultureInvariant);
+        if (m.Success)
+        {
+            yield return null;
+            if (_isCycling)
+            {
+                yield return "sendtochaterror You can't go left or right if the sequence is still cycling!";
+                yield break;
+            }
+            var btn = m.Groups[1].ToString();
+            if (btn == "left")
+                LeftSel.OnInteract();
+            else if (btn == "right")
+                RightSel.OnInteract();
+            yield return new WaitForSeconds(0.1f);
+            yield break;
+        }
+        m = Regex.Match(command, @"^\s*(pause|play|resume|toggle)\s*$", RegexOptions.IgnoreCase | RegexOptions.CultureInvariant);
+        if (m.Success)
+        {
+            yield return null;
+            PlayPauseSel.OnInteract();
+            yield return new WaitForSeconds(0.1f);
+            yield break;
+        }
+        m = Regex.Match(command, @"^\s*(submit)\s*$", RegexOptions.IgnoreCase | RegexOptions.CultureInvariant);
+        if (m.Success)
+        {
+            yield return null;
+            SubmitSel.OnInteract();
+            yield return new WaitForSeconds(0.1f);
+            yield break;
+        }
+        yield break;
+    }
+
+    private IEnumerator TwitchHandleForcedSolve()
+    {
+        ColorPickerSels[0].OnInteract();
+        yield return new WaitForSeconds(0.1f);
+        for (int red = 0; red < 14; red++)
+        {
+            if (_rSegPositions[red] == red)
+                continue;
+            SegmentSels[red].OnInteract();
+            yield return new WaitForSeconds(0.1f);
+            SegmentSels[Array.IndexOf(_rSegPositions, red)].OnInteract();
+            yield return new WaitForSeconds(0.1f);
+        }
+        ColorPickerSels[1].OnInteract();
+        yield return new WaitForSeconds(0.1f);
+        for (int green = 0; green < 14; green++)
+        {
+            if (_gSegPositions[green] == green)
+                continue;
+            SegmentSels[green].OnInteract();
+            yield return new WaitForSeconds(0.1f);
+            SegmentSels[Array.IndexOf(_gSegPositions, green)].OnInteract();
+            yield return new WaitForSeconds(0.1f);
+        }
+        ColorPickerSels[2].OnInteract();
+        yield return new WaitForSeconds(0.1f);
+        for (int blue = 0; blue < 14; blue++)
+        {
+            if (_bSegPositions[blue] == blue)
+                continue;
+            SegmentSels[blue].OnInteract();
+            yield return new WaitForSeconds(0.1f);
+            SegmentSels[Array.IndexOf(_bSegPositions, blue)].OnInteract();
+            yield return new WaitForSeconds(0.1f);
+        }
+        SubmitSel.OnInteract();
+        while (!_moduleSolved)
+            yield return true;
     }
 }
